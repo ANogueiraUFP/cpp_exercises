73b7013a2e66aef160dde19d1b72a4152a7a030c
anogueira <37370@ufp.edu.pt>
2022-03-24 22:18:32 +0000

    adding files

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Andre Nogueira on 24/03/2022.
+//
+#include <iostream>
+#include "hello.h"
+
+using namespace std;
+
+void print_helloworld() {
+
+    cout << "Hello World" << endl;
+    cout<<"I'm learning c++";
+
+}
